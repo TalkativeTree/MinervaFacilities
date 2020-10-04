89114b99e4b135fd4ca1ce24b847c2c6880a572a
Luke <deandrevidal@aol.com>
2020-10-04 15:55:27 -0700

    style push - try again

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -3,11 +3,11 @@ import '../App/index.css';
 
 const LandingPage = () => (
   <div className="container landing-container">
-    {/* <p className="title">Minerva Facilities</p> */}
     <img
       className="landing-logo square fade-in"
       src="https://github.com/ChrisBarnes7404/React-WebBased-MVP/blob/master/public/images/minerva-transparent-vector.png?raw=true"
     />
+    <p className="title">Minerva Facilities</p>
   </div>
 );
 
