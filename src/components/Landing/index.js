05b1c3be30934c8398dd7ef8fcc8d61cd51cc068
Luke <deandrevidal@aol.com>
2020-10-04 15:45:41 -0700

    landing owl

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -3,11 +3,11 @@ import '../App/index.css';
 
 const LandingPage = () => (
   <div className="container landing-container">
-    <img src="../../../public/landing.gif" className="landing-bg" />
-    <p>
-      Style me... someone please.... style me. I can't go on like this
-      anymore
-    </p>
+    {/* <p className="title">Minerva Facilities</p> */}
+    <img
+      className="landing-logo square fade-in"
+      src="https://github.com/ChrisBarnes7404/React-WebBased-MVP/blob/master/public/images/minerva-transparent-vector.png?raw=true"
+    />
   </div>
 );
 
