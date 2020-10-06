0fee8f5018aa1a28a34df4301723fbeee30663fd
David Evans <evansdav.a@gmail.com>
2020-10-06 09:41:42 -0700

    Working on mocking logins for tests

--- tests/Home/index.test.js
+++ tests/Home/index.test.js
@@ -2,6 +2,9 @@ import React, { screen } from 'react'
 import HomePage from '../../src/components/Home/index'
 import { render } from '@testing-library/react-native'
 import { AuthUserContext } from '../../src/components/Session'
+import { Firebase } from '../../src/components/Firebase'
+import firebase from 'firebase/app'
+import { ADMIN_TEST_EMAIL, ADMIN_TEST_PASSWORD, MANAGER_TEST_EMAIL, MANAGER_TEST_PASSWORD, EMPLOYEE_TEST_EMAIL, EMPLOYEE_TEST_PASSWORD } from 'react-native-dotenv'
 
 function renderAuthUserContext (authUser) {
   return render(
@@ -12,9 +15,27 @@ function renderAuthUserContext (authUser) {
 }
 
 describe('showing the correct home page', () => {
-  it('should show admin home page when authUser is an Admin', () => {
-    const authUser = { roles: { ADMIN: 'ADMIN' }, emailVerified: true }
-    renderAuthUserContext(authUser)
+  jest.mock('firebase/app', () => {
+    return {
+      auth: jest.fn(),
+    };
+  });
+  // beforeAll(() => {
+
+  // })
+
+  // afterEach(() => {
+  //   Firebase.doSignOut()
+  // })
+
+  it('should show admin home page when authUser is an Admin', async () => {
+    // Firebase.doSignInWithEmailAndPassword(ADMIN_TEST_EMAIL, ADMIN_TEST_PASSWORD).mockResolvedValue()
+    // const authUser = 
+    // firebase.auth()
+    // renderAuthUserContext(authUser)
+    render(
+      <HomePage />
+    )
     expect(screen.getByText('Admin Homepage').toBeInTheDocument())
   })
 })
