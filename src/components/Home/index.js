57fb7f554513fec54e0f44d5db02f275c8cab676
Luke <deandrevidal@aol.com>
2020-11-09 20:01:24 -0800

    hidden h4

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -35,7 +35,7 @@ const EmployeeHomePage = () => (
     <div className="jumbotron paral paralsec">
       <h1 className="display-8 text-center">Home</h1>
     </div>
-    <h4 className="text-center">Reports List</h4>
+    {/* <h4 className="text-center">Reports List</h4> */}
     
     <Reports />
    
