4745428b78c2c690df5c04ca083e1f7c5f53d16a
Christopher Barnes <chris.barnes.2000@me.com>
2020-11-17 20:40:18 -0800

    [Update]: Firebase Config

--- src/components/Displays/HomePage.js
+++ src/components/Displays/HomePage.js
@@ -2,74 +2,62 @@ import React from 'react';
 import { compose } from 'recompose';
 import { Tabs, Tab } from 'react-bootstrap';
 
-import { AuthUserContext, withAuthorization, withEmailVerification } from '../Session';
+import { AuthUserContext, withEmailVerification, withAuthorization } from '../Session';
 
-import Reports from '../Creators/Reports';
-// import Companies from '../Creators/Companies';
-// import Buildings from '../Creators/Buildings';
-// import Floors from '../Creators/Floors';
-// import FloorDetails from '../Displays/Floors/FloorDetails';
-// import { RoomList, RoomItem } from '../Displays/Rooms';
 
 const HomePage = () => (
-  <EmployeeHomePage />
-  // <AuthUserContext.Consumer>
-  //   {(authUser) =>
-  //     authUser.roles.ADMIN ? (
-  //       <AdminHomePage />
-  //     ) : authUser.roles === 'OWNER' ||
-  //         authUser.roles === 'MANAGER' ? (
-  //       <ManagerHomePage />
-  //     ) : (
-  //       <EmployeeHomePage />
-  //     )
-  //   }
-  // </AuthUserContext.Consumer>
+  <AuthUserContext.Consumer>
+    {(authUser) => (authUser.roles === 'OWNER' || authUser.roles === 'MANAGER' ? <AdminHomePage /> : <EmployeeHomePage />)}
+  </AuthUserContext.Consumer>
 );
 
 const EmployeeHomePage = () => (
-  <div >
+  <div>
     <div className="jumbotron paral paralsec">
       <h1 className="display-8 text-center">Home</h1>
     </div>
     <h6 className="text-center">Recent Reports:</h6>
-    <Reports />
+    {/* <Reports /> */}
   </div>
 );
 
-// const AdminHomePage = () => (
-//   <div>
-//     <div className="jumbotron paral paralsec">
-//       <h1 className="display-8 text-center">Admin Homepage</h1>
-//     </div>
-//     <div className="tab-wrapper">
-//       <div className="container">
-//         <div className="row">
-//           <Tabs
-//             defaultActiveKey="companies"
-//             className="non-nav"
-//             variant="pills"
-//           >
-//             <Tab eventKey="companies" title="Companies">
-//               <div className="tab-item-wrapper">
-//                 <br></br>
-//                 <h4 className="text-center">Companies List</h4>
-//                 <Companies />
-//               </div>
-//             </Tab>
-//             <Tab eventKey="reports" title="Reports">
-//               <div className="tab-item-wrapper">
-//                 <br></br>
-//                 <h4 className="text-center">Reports List</h4>
-//                 <Reports />
-//               </div>
-//             </Tab>
-//           </Tabs>
-//         </div>
-//       </div>
-//     </div>
-//   </div>
-// );
+
+const AdminHomePage = () => (
+  <div>
+    <div className="jumbotron paral paralsec">
+      <h1 className="display-8 text-center">Admin Homepage</h1>
+    </div>
+    <h6 className="text-center">Recent Reports:</h6>
+    {/* <Reports /> */}
+
+    {/* <div className="tab-wrapper">
+      <div className="container">
+        <div className="row">
+          <Tabs
+            defaultActiveKey="companies"
+            className="non-nav"
+            variant="pills"
+          >
+            <Tab eventKey="companies" title="Companies">
+              <div className="tab-item-wrapper">
+                <br></br>
+                <h4 className="text-center">Companies List</h4>
+                <Companies />
+              </div>
+            </Tab>
+            <Tab eventKey="reports" title="Reports">
+              <div className="tab-item-wrapper">
+                <br></br>
+                <h4 className="text-center">Reports List</h4>
+                <Reports />
+              </div>
+            </Tab>
+          </Tabs>
+        </div>
+      </div>
+    </div> */}
+  </div>
+);
 
 const condition = (authUser) => !!authUser;
 
--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,36 +1,17 @@
 import app from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/database';
-
-// const config = {
-//   apiKey: process.env.REACT_APP_API_KEY,
-//   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-//   databaseURL: process.env.REACT_APP_DATABASE_URL,
-//   projectId: process.env.REACT_APP_PROJECT_ID,
-//   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-//   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-// };
-
-// const config = {
-//   apiKey: 'AIzaSyDl_Mionn-j0hp8f4hriSZqvOI9q754zCA',
-//   authDomain: 'taddle-new-harmony-cafe.firebaseapp.com',
-//   databaseURL: 'https://taddle-new-harmony-cafe.firebaseio.com',
-//   projectId: 'taddle-new-harmony-cafe',
-//   storageBucket: 'taddle-new-harmony-cafe.appspot.com',
-//   messagingSenderId: '183816749191',
-//   appId: '1:183816749191:web:8677931d232f454a767822',
-//   measurementId: 'G-EG3DEKSHRE',
-// };
+import 'firebase/analytics';
 
 const config = {
-  apiKey: 'AIzaSyBnJSs5VkOG4k2VAByyYU8pxeyOthF3FFs',
-  authDomain: 'minerva-facilities.firebaseapp.com',
-  databaseURL: 'https://minerva-facilities.firebaseio.com',
-  projectId: 'minerva-facilities',
-  storageBucket: 'minerva-facilities.appspot.com',
-  messagingSenderId: '286455586521',
-  appId: '1:286455586521:web:658ab0b3a18a1dcebfe8c8',
-  measurementId: 'G-674WMEGJDS',
+  apiKey: process.env.REACT_APP_API_KEY || 'AIzaSyBnJSs5VkOG4k2VAByyYU8pxeyOthF3FFs',
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN || 'minerva-facilities.firebaseapp.com',
+  databaseURL: process.env.REACT_APP_DATABASE_URL || 'https://minerva-facilities.firebaseio.com',
+  projectId: process.env.REACT_APP_PROJECT_ID || 'minerva-facilities',
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET || 'minerva-facilities.appspot.com',
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID || '286455586521',
+  appId: process.env.REACT_APP_APP_ID || '1:286455586521:web:658ab0b3a18a1dcebfe8c8',
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID || 'G-674WMEGJDS',
 };
 
 
@@ -39,24 +20,21 @@ class Firebase {
     app.initializeApp(config);
 
     /* Helper */
-
     this.serverValue = app.database.ServerValue;
     this.emailAuthProvider = app.auth.EmailAuthProvider;
 
     /* Firebase APIs */
-
     this.auth = app.auth();
     this.db = app.database();
+    this.analytics = app.analytics();
 
     /* Social Sign In Method Provider */
-
     this.googleProvider = new app.auth.GoogleAuthProvider();
     this.facebookProvider = new app.auth.FacebookAuthProvider();
     this.twitterProvider = new app.auth.TwitterAuthProvider();
   }
 
   // *** Auth API ***
-
   doCreateUserWithEmailAndPassword = (email, password) =>
     this.auth.createUserWithEmailAndPassword(email, password);
 
@@ -83,11 +61,12 @@ class Firebase {
         process.env.REACT_APP_CONFIRMATION_EMAIL_REDIRECT ||
         'http://localhost:19006/',
     });
+
   doPasswordUpdate = (password) =>
     this.auth.currentUser.updatePassword(password);
 
-  // *** Merge Auth and DB User API *** //
 
+  // *** Merge Auth and DB User API *** //
   onAuthUserListener = (next, fallback) =>
     this.auth.onAuthStateChanged((authUser) => {
       if (authUser) {
@@ -117,24 +96,20 @@ class Firebase {
       }
     });
 
+
   // *** User API ***
-
   user = (uid) => this.db.ref(`users/${uid}`);
-
   users = () => this.db.ref('users');
-
   setUserCompany = (userID, companyID) => this.user(userID).update({company_id: companyID});
 
+
   // *** Report API ***
-
   report = (uid) => this.db.ref(`reports/${uid}`);
-
   reports = () => this.db.ref('reports');
 
+
   // **** Company API ***
-
   company = (uid) => this.db.ref(`companies/${uid}`);
-
   companies = () => this.db.ref('companies');
 
   createCompany = (companyData) => {
@@ -150,37 +125,21 @@ class Firebase {
     this.db.ref().update(updates);
   };
 
+
   // **** Building API ***
-
   building = (uid) => this.db.ref(`buildings/${uid}`);
-
   buildings = () => this.db.ref('buildings');
 
-  // building = (companyId, uid) =>
-  //   this.db.ref(`companies/${companyId}/buildings/${uid}`);
-
-  // buildings = (companyId) =>
-  //   this.db.ref(`companies/${companyId}/buildings`);
 
   // **** Floors API ***
-
-  // floor = (companyId, buildingId, uid) =>
-  //   this.db.ref(
-  //     `companies/${companyId}/buildings/${buildingId}/floors/${uid}`,
-  //   );
-
-  // floors = (companyId, buildingId) =>
-  //   this.db.ref(
-  //     `companies/${companyId}/buildings/${buildingId}/floors`,
-  //   );
-
   floor = (uid) => this.db.ref(`floors/${uid}`);
-
   floors = () => this.db.ref('floors');
 
-  room = (uid) => this.db.ref(`rooms/${uid}`);
 
+  // *** Rooms API ***
+  room = (uid) => this.db.ref(`rooms/${uid}`);
   rooms = () => this.db.ref('rooms');
+
 }
 
 export default Firebase;
