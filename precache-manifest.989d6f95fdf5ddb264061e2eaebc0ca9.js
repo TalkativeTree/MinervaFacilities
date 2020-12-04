e938b66ffa4c81bf6424faa8cba847a33a244a92
Christopher Barnes <chris.barnes.2000@me.com>
2020-12-03 16:05:25 -0800

    Updates

---
+++ precache-manifest.989d6f95fdf5ddb264061e2eaebc0ca9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "0ae0a2e3b897077985f1",
+    "url": "/MinervaFacilities/static/js/app.30a31d42.chunk.js"
+  },
+  {
+    "revision": "08b6be5cb371fdcde7d4",
+    "url": "/MinervaFacilities/static/js/runtime~app.b6b603b6.js"
+  },
+  {
+    "revision": "6278f1532ba349217e7b",
+    "url": "/MinervaFacilities/static/js/2.74046740.chunk.js"
+  },
+  {
+    "revision": "65401fc6f6bfe917c8c0a437b5b40637",
+    "url": "/MinervaFacilities/static/media/background_blue_gradient.ba929207.jpg"
+  },
+  {
+    "revision": "06865c080e50d3f25bac757172242455",
+    "url": "/MinervaFacilities/static/media/trianglify-lowres.879ffb6b.png"
+  },
+  {
+    "revision": "6f73f6266f4321f43022becfa19116fb",
+    "url": "/MinervaFacilities/index.html"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "/MinervaFacilities/serve.json"
+  },
+  {
+    "revision": "2002987acac9deb6803ab4ba381226ba",
+    "url": "/MinervaFacilities/expo-service-worker.js"
+  },
+  {
+    "revision": "6c3cf372b8f32d95d480de0f0e2b687d",
+    "url": "/MinervaFacilities/register-service-worker.js"
+  },
+  {
+    "revision": "ad77c937a3ae2fb03f274463a4817363",
+    "url": "/MinervaFacilities/static/js/2.74046740.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14f34b37a2c03fd6fb209ff9443f13fb",
+    "url": "/MinervaFacilities/manifest.json"
+  }
+];
---
+++ register-service-worker.js
@@ -0,0 +1,1 @@
+"serviceWorker"in navigator&&window.addEventListener("load",(function(){navigator.serviceWorker.register("/MinervaFacilities/expo-service-worker.js",{scope:"/MinervaFacilities/"}).then((function(e){})).catch((function(e){console.info("Failed to register service-worker",e)}))}));
